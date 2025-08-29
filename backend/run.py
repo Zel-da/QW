6c1aa44a6bb39e3955ed9ee3372ebf998d899240
YeJoon Ahn <66552780+Zel-da@users.noreply.github.com>
2025-08-29 15:46:41 +0900

    ㄴ

--- backend/run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    # Use waitress or gunicorn for production
-    # For development:
-    app.run(debug=True, port=5000)
